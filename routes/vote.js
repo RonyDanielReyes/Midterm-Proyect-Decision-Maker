a55e72e297869c1b4cde560ba8bb7c3eeff5610b
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2023-11-29 01:40:49 -0800

    adding routes in the vote.js file

--- routes/vote.js
+++ routes/vote.js
@@ -1,9 +1,66 @@
 const express = require('express');
 const router  = express.Router();
+const choicesQueries = require('../db/queries/choices');
+const pollsQueries = require('../db/queries/polls');
+const votesQueries = require('../db/queries/votes');
+const mailNewVote = require('../public/scripts/mail').mailNewVote;
 
-router.get('/', (req, res) => {
-  res.render('vote');
+router.get('/:id', (req, res) => {
+  pollsQueries.getPollByLink(req.params.id)
+    .then(poll => {
+      choicesQueries.getChoices(poll.id)
+        .then((choices) => {
+          const templateVars = {
+            poll,
+            choices
+          };
+          res.render('vote', templateVars);
+        });
+    })
+    .catch(err => {
+      res.render('msg', {msg: err.message});
+    });
+
+});
+
+
+router.post('/:id', (req, res) => {
+  const voter_name = req.body.voter;
+  return pollsQueries.getPollByLink(req.params.id)
+    .then(async(poll) => {
+      console.log("------------",req.body,"---------",req,"++++++");
+      console.log(poll);
+      for (const choice_id in req.body) {
+        if (choice_id !== 'voter') {
+          const vote = {
+            poll_id: poll.id,
+            choice_id: Number(choice_id),
+            ranking: req.body[choice_id],
+            voter_name
+          };
+          await votesQueries.addVote(vote)
+            .catch(err => {
+              res
+                .status(500)
+                .json({ error: err.message });
+            });
+        }
+      }
+      let mailName = voter_name
+      if (!voter_name) {
+        mailName = 'Someone';
+      }
+      mailNewVote(poll, mailName).catch(console.error);
+    })
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+
 });
 
 module.exports = router;
-
