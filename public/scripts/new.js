4a0cd20007353ba9ca374c3a6beffc0ee53465e0
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2023-11-29 01:43:01 -0800

    adding the button to new

---
+++ public/scripts/new.js
@@ -0,0 +1,16 @@
+$(() => {
+  $('.button').on('click', () => {
+    $.ajax({
+      method: 'POST',
+      url: '/api/new'
+    })
+      .done((response) => {
+        const $pollsList = $('#polls');
+        $pollsList.empty();
+
+        for (const poll of response.polls) {
+          $(`<li class="polls">`).text(`${poll.title}: ${poll.description}`).appendTo($pollsList);
+        }
+      });
+  });
+});
