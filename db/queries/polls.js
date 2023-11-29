cd174ad0de404583e584f0246959e2324f508315
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2023-11-29 02:00:22 -0800

    adding the getpollid querie

--- db/queries/polls.js
+++ db/queries/polls.js
@@ -12,5 +12,16 @@ const getPolls = () => {
     });
 };
 
+/**
+ * Get poll with given id from the database.
+ * @param id poll id.
+ * @return {Promise<{}>} A promise to the user.
+ */
+const getPollById = (id) => {
+  return db.query('SELECT * FROM polls WHERE id = $1;', [id])
+    .then(data => {
+      return data.rows[0];
+    });
+};
 
-module.exports = { getPolls };
+module.exports = { getPolls, getPollById };
