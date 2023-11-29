baad122ad4acd757bcc60f5d3dbdcf7d456a83fb
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2023-11-29 01:45:23 -0800

    adding the app js file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,30 @@
 // Client facing scripts here
+$(()=>{
+  $("#refresh_btn").on('click',(event)=>{
+    event.preventDefault();
+    window.location.reload();
+    // $.ajax({
+    //   method:"GET",
+    //   url:$("#admintoken").text(),
+    //   success:(data)=>{
+    //     console.log("here",data);
+    //   }
+    // })
+  });
+
+
+  $("#close_btn").on('click',(event)=>{
+    event.preventDefault();
+    const link = $("#admintoken").text().concat("/stop");
+    $.ajax({
+      method:"POST",
+      url:link,
+      success:((data)=>{
+        // console.log(data);
+        window.location.href = "/";
+      })
+
+    })
+
+  });
+})
